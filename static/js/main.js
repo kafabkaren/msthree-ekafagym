4dce443c731385eb2b452edf5f22792cab167ca6
kafamem <kafamem@gmail.com>
2021-06-16 17:27:23 +0000

    Styling and accordion setup

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
+    // Sidenav position
     $('.sidenav').sidenav({edge: "right"});
+    // Initializing Collapsible feature
+    $('.collapsible').collapsible();
   });
+
+
